64267917af677a0fc8325c36630e9be56a74f2a5
unknown <santiagoparramuri@gmail.com>
2024-05-11 16:49:15 -0500

    Ultimos Cambios

--- funciones.js
+++ funciones.js
@@ -14,3 +14,18 @@ function cerrarModal() {
 
 // Asociar la función mostrarFormulario al evento click del botón
 botonRegistro.onclick = mostrarFormulario;
+document.addEventListener("DOMContentLoaded", function() {
+  var botonNoticias = document.getElementById("botonNoticias");
+  var seccionNoticias = document.getElementById("noticias");
+
+  botonNoticias.addEventListener("click", function(event) {
+      event.preventDefault(); // Evitar que el enlace del botón realice una acción predeterminada
+      // Alternar la visibilidad de la sección de noticias
+      if (seccionNoticias.style.display === "block") {
+          seccionNoticias.style.display = "none"; // Ocultar la sección de noticias si ya está visible
+      } else {
+          seccionNoticias.style.display = "block"; // Mostrar la sección de noticias si está oculta
+      }
+  });
+});
+
